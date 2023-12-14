7f8eb820679558a961d4fdde2a83a95afb604fcb
origin <https://github.com/jeromario/desafio_poo.git>
2023-12-14 12:06:15 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "123", modelo: "S2", imei: "k1k2", memoria: 64);
+Smartphone iphone =  new Iphone(numero: "333", modelo: "14 PRO", imei: "1v2v", memoria: 128);
+Console.WriteLine("Smartphone Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+Console.WriteLine();
+Console.WriteLine("Smartphone Iphone:");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Deezer");
